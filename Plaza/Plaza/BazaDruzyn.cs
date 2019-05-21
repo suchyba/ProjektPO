403fa994e543ffd5e3b71cc8d6cf832bb732fc29
suchyba.bq@gmail.com <suchyba.bq@gmail.com>
2019-05-21 13:28:38 +0200

    Dodałem plik BazaDrużyn.cs i wypełniłem go dodałem plik Mecz.cs

---
+++ Plaza/Plaza/BazaDruzyn.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plaza
+{
+    class BazaDruzyn
+    {
+        List<Druzyna> listaDruzyn;
+        List<Mecz> listaMeczy;
+
+        public BazaDruzyn()
+        {
+            listaDruzyn = new List<Druzyna>();
+            listaMeczy = new List<Mecz>();
+        }
+
+        public void ZglosDruzyne(Druzyna d)
+        {
+            listaDruzyn.Add(d);
+        }
+        public void WycofajDruzyne(string nazwa)
+        {
+            listaDruzyn.Remove(listaDruzyn.Where(d => d.GetNazwa() == nazwa).FirstOrDefault());
+        }
+        public void GenerujFinaly()
+        {
+
+        }
+        public void GenerujPolfinaly()
+        {
+
+        }
+    }
+}
--- Plaza/Plaza/MainWindow.xaml.cs
+++ Plaza/Plaza/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace Plaza
         public MainWindow()
         {
             InitializeComponent();
+            BazaDruzyn baza = new BazaDruzyn();
         }
     }
 }
